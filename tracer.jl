3e2148cfa4f5b72052825df4feaea997f26ea898
Madhumitha Viswanathan <madhuvishy@gmail.com>
2014-03-27 15:18:37 -0400

    Added intersection logic for plane

--- tracer.jl
+++ tracer.jl
@@ -24,7 +24,8 @@ type Intersection
 	object
 end
 
-##Follows pseudocode from https://www.cs.unc.edu/~rademach/xroads-RT/RTarticle.html ##
+## Method to calculate the Intersection of a ray on a sphere ##
+## Follows pseudocode from https://www.cs.unc.edu/~rademach/xroads-RT/RTarticle.html ##
 function intersection(s::Sphere, l::Ray)
 	v = dot(l.direction, (l.origin - s.center))
 	disc = s.radius ^ 2 - (dot(l.origin - s.center, l.origin - s.center) - v ^ 2)
@@ -38,9 +39,20 @@ function intersection(s::Sphere, l::Ray)
 	end
 end
 
+## Method to calculate Intersection of a ray on a plane ##
+function intersection(p::Plane, l::Ray)
+	v = dot(l.direction, p.normal)
+	if v == 0
+		return Intersection(Vector(), -1, Vector(), p)
+	else
+		d = dot(p.point - l.origin, p.normal) / v
+		return Intersection (l.origin + (l.direction * d), d, p.normal, p)
+	end
+end
+
 
 
 r = Ray(Vector(0, 1, 2), Vector(1, 1, 1))
 s = Sphere(Vector(0, 0, 0), 5, Vector(255, 255, 255))
-
-println(intersection(s, r))
+p = Plane( Vector(0,0,-12), Vector(0,0,1), Vector(255,255,255))
+println(intersection(p, r))
